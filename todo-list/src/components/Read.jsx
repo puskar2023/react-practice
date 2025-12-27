2cc0dd3732da899c6900ccbeda93aad979eb9dc2
puskar2023 <puskarmandal04@gmail.com>
2025-12-27 19:04:19 +0530

    responsive

--- todo-list/src/components/Read.jsx
+++ todo-list/src/components/Read.jsx
@@ -14,13 +14,13 @@ const Read = () => {
         toast.error("Todos deleted!")
     }
     const rendertasks = todos.map(todo => {
-        return <li key={todo.id} className='p-3 flex justify-between bg-black'>
+        return <li key={todo.id} className='p-3 flex justify-between bg-black md:text-2xl'>
             <span>{todo.title}</span>
             <button onClick={()=>updatetodos(todo.id)} className='text-red-400'>Delete</button>
         </li>
     })
   return (
-    <div className='w-[100%] h-[50%] overflow-auto px-6 py-3 border flex flex-col gap-1 lg:w-[35%] h-[90%] lg:px-15 py-10 lg:gap-10'>
+    <div className='w-[100%] h-[50%] overflow-auto px-6 py-3 border flex flex-col gap-1 lg:w-[40%] h-[90%] lg:px-15 py-10 lg:gap-10'>
         <h1 className='text-4xl'>All Pending Tasks</h1>
         <ol className='flex flex-col gap-2 mt-10 lg:mt-1 '>{rendertasks}</ol>
     </div>
