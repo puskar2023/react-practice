ab988262ac9378841c442be6355bf703724c2a0f
puskar2023 <puskarmandal04@gmail.com>
2026-01-06 22:56:47 +0530

    changing in style

--- recipe-application/src/components/RecipeCard.jsx
+++ recipe-application/src/components/RecipeCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const RecipeCard = (props) => {
     const {id, image, title, desc} = props.recipe;
   return (
-    <Link to={`/recipes/details/${id}`} className="h-100 border p-3 rounded-2xl flex flex-col gap-1">
+    <Link to={`/recipes/details/${id}`} className="h-100 border p-3 bg-black/80 rounded-2xl flex flex-col gap-1">
       <div className='w-[100%] h-[65%] overflow-hidden rounded-2xl'>
       <img className="h-full w-full object-cover transition duration-300 hover:scale-110" src={image} alt="recipe image" /> </div>
       <h1 className="text-xl font-bold">{title}</h1>
--- recipe-application/src/pages/Recipes.jsx
+++ recipe-application/src/pages/Recipes.jsx
@@ -9,7 +9,7 @@ const Recipes = () => {
     return <RecipeCard key={recipe.id} recipe={recipe} />
   })
   return (
-    <div className="w-[100%] pt-25 px-5 pb-3 grid grid-cols-1 gap-7 lg:px-19 lg:grid-cols-3">
+    <div className="w-[100%] min-h-screen pt-25 px-5 pb-3 bg-[url('https://images.unsplash.com/photo-1623129090933-f818be5272e8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover grid grid-cols-1 gap-7 lg:px-19 lg:grid-cols-3">
       {renderrecipes}
     </div>
   )
