d700eebfd774891abbfe5b9d8ca24511f16e500b
puskar2023 <puskarmandal04@gmail.com>
2026-01-06 22:41:25 +0530

    adding style

--- recipe-application/src/components/RecipeCard.jsx
+++ recipe-application/src/components/RecipeCard.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 const RecipeCard = (props) => {
     const {id, image, title, desc} = props.recipe;
   return (
-    <Link to={`/recipes/details/${id}`} className="h-100 border p-3 rounded flex flex-col gap-1">
-      <img className="w-[100%] h-[65%] object-cover" src={image} alt="recipe image" />
+    <Link to={`/recipes/details/${id}`} className="h-100 border p-3 rounded-2xl flex flex-col gap-1">
+      <div className='w-[100%] h-[65%] overflow-hidden rounded-2xl'>
+      <img className="h-full w-full object-cover transition duration-300 hover:scale-110" src={image} alt="recipe image" /> </div>
       <h1 className="text-xl font-bold">{title}</h1>
       <p className="">{desc?.slice(0,120) + "..." }<small className='text-blue-400'>more</small></p>
    </Link>
