7753d0c71e03c2a5baa4db453a7d065f5553cce5
puskar2023 <puskarmandal04@gmail.com>
2026-01-08 23:10:39 +0530

    add style

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { UserPlus, MessageCircleDashed } from 'lucide-react';
 const App = () => {
   return (
     <div className='h-screen w-full flex'>
@@ -7,6 +7,10 @@ const App = () => {
       <div className='right-bar h-screen w-[80%] bg-black/86'>
       <div className="nav h-15 flex items-center justify-between px-6 bg-gradient-to-r from-[#z4D2B8C] to-[#85409D]">
         <h1 className='text-2xl text-white/80'>ChatGPT</h1>
+        <div className='flex items-center gap-10 text-white px-7'>
+          <UserPlus className='text-xl' />
+          <MessageCircleDashed className='text-xl'/>
+        </div>
       </div>
       <div className="container"></div>
       <div className="input"></div>
