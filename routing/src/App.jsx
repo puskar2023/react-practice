ac05df66e645db4892b94153e5c90f2db8b1a143
puskar2023 <puskarmandal04@gmail.com>
2026-01-08 23:03:00 +0530

    making UI of chatbot

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import Nav from './components/Nav'
-import Mainroutes from './routes/Mainroutes'
-import Home from './components/Home'
-import Products from './components/Products'
-import Service from './components/Service'
-import About from './components/About'
-
 
 const App = () => {
   return (
-    <div className='min-h-screen w-full bg-black'>
-      <Nav />
-      <Home />
-      <Products />
-      <Service />
-      <About />
+    <div className='h-screen w-full flex'>
+      <div className='left-bar h-screen w-[20%] bg-black'></div>
+      <div className='right-bar h-screen w-[80%] bg-black/86'>
+      <div className="nav h-15 flex items-center justify-between px-6 bg-gradient-to-r from-[#z4D2B8C] to-[#85409D]">
+        <h1 className='text-2xl text-white/80'>ChatGPT</h1>
+      </div>
+      <div className="container"></div>
+      <div className="input"></div>
+      </div>
     </div>
   )
 }
--- routing/src/routes/Mainroutes.jsx
+++ routing/src/routes/Mainroutes.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../components/Home'
-import Products from '../components/Products'
-import Service from '../components/Service'
-import About from '../components/About'
-import Create from '../components/Create'
-import ProductDetails from '../components/ProductDetails'
-import ServiceDetails from '../components/ServiceDetails'
 
-const Router = () => {
+const Mainroutes = () => {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/products/details/:name' element={<ProductDetails />} />
-        <Route path='/service' element={<Service />} >
-        <Route path='/service/details' element={<ServiceDetails />} />
-        </Route>
-        <Route path='/about' element={<About />} />
-        <Route path='/create' element={<Create />} />
-      </Routes>
-    </div>
+    <div>Mainroutes</div>
   )
 }
 
-export default Router
+export default Mainroutes
